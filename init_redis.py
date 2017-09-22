f9f6d32d95e9d8ed88bf2b2b21d8ec0c751ac725
Xia Jun <xiajun@XiadeMacBook.local>
2017-09-22 21:22:00 +0800

    实现初始化数据库

---
+++ init_redis.py
@@ -0,0 +1,34 @@
+from config import config
+from redis import StrictRedis
+import json
+
+Redis2Info = config.info["Redis2Info"]
+redis_db = StrictRedis(
+    host=Redis2Info['host'],
+    port=Redis2Info['port'],
+    password=Redis2Info['pwd'],
+    db=Redis2Info['db']
+)
+
+
+# catalogs = redis_db.smembers("kejiliechannels")
+
+# # print (catalogs)
+# catalist = []
+# for c in catalogs:
+#     catalist.append(c.decode('utf-8'))
+# cata_str = json.dumps(catalist)
+
+# fp = open('./urllist.txt', 'w')
+# fp.write(cata_str)
+
+
+fp = open('./urllist.txt', 'r')
+init_db_content = fp.read()
+
+cat = json.loads(init_db_content)
+
+aset = set()
+for c in cat:
+    redis_db.sadd("kejiliechannels", json.dumps(
+        {'url': response.url, 'title': title, 'logo': logo}))
